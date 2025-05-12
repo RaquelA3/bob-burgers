0c19e3ab3b2f10857d5146f4a118c279bdcc272c
Raquel Amezcua <raquelh6h@gmail.com>
2025-05-11 20:44:03 -0600

    pruba4

---
+++ src/app/inicio/page.jsx
@@ -0,0 +1,42 @@
+import { useRouter } from 'next/navigation'; 
+import '../inicio.css';
+import Image from 'next/image'; 
+
+export default Inicio() {
+  const router = useRouter();  // Sirve para navegar entre las páginas
+
+  const imagenes = {
+    iconotemporada: "../img/icono2.png",
+  };
+
+  return (
+    <div className='bob-container'>
+      <h1 className='titulo-principal'>BOB BURGERS</h1>
+
+      <div className='form-temporada'>
+        <Image
+          className='icon'
+          src={imagenes.iconotemporada}
+          alt='icono temporada'
+          width={50}   // Asegúrate de poner el tamaño adecuado
+          height={50}  // Asegúrate de poner el tamaño adecuado
+        />
+        <span className='temp'>14 temporada</span>
+      </div>
+      
+      <div className='form-group'>
+         <label className='registro'>Registrarse</label> 
+         <label className='inicio'>Iniciar sesión</label> 
+      </div>
+      <div className='boton-container'>
+        <button className='botonComenzar' 
+          onClick={() => router.push('/base')}  
+        >
+           Comenzar a ver
+        </button>
+      </div>
+    
+    </div>
+  );
+}
+
