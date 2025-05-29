8febcddd89c0116086bf6a562d4008db17d30602
Raquel Amezcua <raquelh6h@gmail.com>
2025-05-29 00:58:37 -0600

    CCambio

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,10 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      "@next/next/no-img-element": "off",
     },
   }),
 ]
  
 export default eslintConfig
+
