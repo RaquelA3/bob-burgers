75d4421425bcf9471b6e5c7aa16c98253c01c4f1
Raquel Amezcua <raquelh6h@gmail.com>
2025-05-29 00:54:42 -0600

    Cambio en eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,18 @@
-/* import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+ 
 const eslintConfig = [
-  // Extiende las reglas básicas de Next.js
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      // Permite usar <img> en lugar de <Image> de Next.js
-      "@next/next/no-img-element": "off",
-
-      // Desactiva el error por usar comillas no escapadas en JSX
-      "react/no-unescaped-entities": "off",
-
-      // Desactiva el warning por no usar "alt" en imágenes
-      "jsx-a11y/alt-text": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
-  },
-];
-
-export default eslintConfig;
- */
+  }),
+]
+ 
+export default eslintConfig
